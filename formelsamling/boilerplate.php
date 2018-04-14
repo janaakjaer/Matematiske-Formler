f7df2c795a54b8541072aaa5f9f0e8cdc1376b3c
Jan Aakjaer <info@jan-aakjaer.dk>
2018-04-14 15:12:55 +0200

    Updated boilerplate

--- formelsamling/boilerplate.php
+++ formelsamling/boilerplate.php
@@ -6,14 +6,14 @@ Jan Aakjær 2018
 <html lang="da">
     <head>
         <meta charset="UTF-8">
-        <title>Matematiske formler</title>
+        <title>Boilerplate</title>
         <meta name="description" content="Dette projekt skal gøre det lettere at beregne ved hjælp af matematiske formler.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/styles.css">
     </head>  <!-- HEAD -->
     <body>
         <section>
-            <h2>Matematiske formler</h2>
+            <h1>Boilerplate</h1>
         </section>
         <?php
         // put your code here
