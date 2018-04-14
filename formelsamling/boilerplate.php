04e2bfc72aa08174d022871f70876e05e8328359
Jan Aakjaer <info@jan-aakjaer.dk>
2018-04-14 15:00:40 +0200

    initial commit

---
+++ formelsamling/boilerplate.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Dette projekt skal gøre det lettere at beregne ved hjælp af matematiske formler.
+Jan Aakjær 2018
+-->  <!-- KOMMENTAR -->  
+<html lang="da">
+    <head>
+        <meta charset="UTF-8">
+        <title>Matematiske formler</title>
+        <meta name="description" content="Dette projekt skal gøre det lettere at beregne ved hjælp af matematiske formler.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>  <!-- HEAD -->
+    <body>
+        <section>
+            <h2>Matematiske formler</h2>
+        </section>
+        <?php
+        // put your code here
+        ?>
+    </body>  <!-- BODY -->
+</html>  <!-- HTML -->
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+Dette projekt skal gøre det lettere at beregne ved hjælp af matematiske formler.
+Jan Aakjær 2018
+-->  <!-- KOMMENTAR -->  
+<html lang="da">
+    <head>
+        <meta charset="UTF-8">
+        <title>Matematiske formler</title>
+        <meta name="description" content="Dette projekt skal gøre det lettere at beregne ved hjælp af matematiske formler.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>  <!-- HEAD -->
+    <body>
+        <section>
+            <h1>Matematiske formler</h1>
+            <h2>Økonomi</h2>
+            <ul>
+                <li> <a href="formelsamling/economy-procent.php">Procent</a> </li>
+                <li> <a href="formelsamling/economy-interest">Rente</a> </li>
+                <li> <a href="formelsamling/economy-growth">Vækst</a> </li>
+                <li> <a href="formelsamling/economy-currency">Fremmed valuta</a> </li>
+                
+            </ul>
+            <h2>Geometri</h2>
+            <ul>
+                <li> <a href="formelsamling/geometri-begreb.php">Begreber</a> </li>
+                <li> <a href="formelsamling/geometri-areal">Areal</a> </li>
+                <li> <a href="formelsamling/geometri-rumfang">Rumfang og overflade</a> </li>
+                <li> <a href="formelsamling/geometri-flytninger">Flytninger</a> </li>
+                <li> <a href="formelsamling/geometri-tegning">Tegning</a> </li>
+            </ul>
+           
+        </section>
+        <?php
+        // put your code here
+        ?>
+    </body>  <!-- BODY -->
+</html>  <!-- HTML -->
